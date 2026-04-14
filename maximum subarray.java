cb4010ee8426ce261cfda48e4c6406dcc2164903
Karthigaipriya2005 <nkarthigaipriya2005@gmail.com>
2026-04-14 22:51:08 +0530

    Implement maximum subarray solution

---
+++ maximum subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentsum = 0;
+        int maxsum = Integer.MIN_VALUE;
+        for(int i=0;i < nums.length;i++){
+            currentsum = currentsum + nums[i];
+            maxsum = Math.max(currentsum,maxsum);
+            if(currentsum < 0){
+                currentsum = 0;
+            }
+        }
+        return maxsum;
+    }
+}
